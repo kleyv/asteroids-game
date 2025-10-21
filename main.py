1f4906534e8cb42fab93b269fcf882ba907e7456
user <user@mail.com>
2025-10-21 10:19:33 +0100

    Add game loop and draw game window with pygame api

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    counter = 0
+    while counter < 2000:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill('black')
+        counter += 1
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
