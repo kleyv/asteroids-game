e5f24195d8a2b496e12c4db1d83ebe7d34f17ad2
user <user@mail.com>
2025-10-21 11:49:50 +0100

    Group drawable and updatable objects together

--- main.py
+++ main.py
@@ -11,9 +11,12 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
-
     player = Player(x,y)
     # counter = 0
     # while counter < 2000:
@@ -22,8 +25,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
-        player.draw(screen)
-        player.update(dt)
+
+        for object in drawable:
+            object.draw(screen)
+        
+        updatable.update(dt)
         # counter += 1
         pygame.display.flip()
         dt = clock.tick(60) / 1000
